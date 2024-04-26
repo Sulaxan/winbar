56d715461551a2df38d1d9e01aa3fc7affbb778f
Sulaxan <sulaxan33@yahoo.ca>
2024-04-26 15:02:55 -0400

    implement client

--- winbar/src/client.rs
+++ winbar/src/client.rs
@@ -1,22 +1,55 @@
+use std::sync::Arc;
+
 use anyhow::Result;
-use tokio::net::TcpStream;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+    sync::mpsc::Receiver,
+    task::JoinHandle,
+};
 
-use crate::protocol::WinbarClientPayload;
+use crate::protocol::{WinbarClientPayload, WinbarServerPayload};
 
-pub struct WinbarClient {}
+pub struct WinbarClient {
+    response_handler: Arc<dyn Fn(WinbarClientPayload) + Sync + Send>,
+    stream_handle: Option<JoinHandle<()>>,
+}
 
 impl WinbarClient {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(response_handler: Arc<dyn Fn(WinbarClientPayload) + Sync + Send>) -> Self {
+        Self {
+            response_handler,
+            stream_handle: None,
+        }
     }
 
-    pub async fn start(addr: &str) {
-        let mut stream = TcpStream::connect(addr).await;
-    }
+    pub async fn start(
+        &mut self,
+        addr: &str,
+        mut recv: Receiver<WinbarServerPayload>,
+    ) -> Result<()> {
+        let mut stream = TcpStream::connect(addr).await?;
+        let response_handler = self.response_handler.clone();
 
-    pub async fn send() -> Result<WinbarClientPayload> {
-        // tokio::select! to wait on the payload or error out if we received nothing within a few
-        // seconds
-        todo!()
+        self.stream_handle = Some(tokio::spawn(async move {
+            let mut buf = vec![0; 4096];
+            let (mut rx, mut wx) = stream.split();
+
+            loop {
+                tokio::select! {
+                    read_bytes = rx.read(&mut buf) => {
+                        let read_bytes = read_bytes.unwrap();
+                        let payload = serde_json::from_slice::<WinbarClientPayload>(&buf[..read_bytes]).unwrap();
+                        response_handler(payload);
+                    }
+                    payload = recv.recv() => {
+                        let payload = payload.unwrap();
+                        wx.write_all(&serde_json::to_vec(&payload).unwrap()).await.unwrap();
+                    }
+                }
+            }
+        }));
+
+        Ok(())
     }
 }
